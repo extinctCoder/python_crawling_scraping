dff690831179b46e5982f0ed5d3946ef376a797d
fiery.snowflake <extinctCoder@outlook.com>
2024-03-12 23:13:03 +0000

    Mar 13, 2024, 5:13 AM

--- src/product_scraper/spiders/book.py
+++ src/product_scraper/spiders/book.py
@@ -17,11 +17,6 @@ class BookSpider(scrapy.Spider):
                     book_url = "https://books.toscrape.com/catalogue/" + relative_url
 
                 yield response.follow(book_url, callback=self.parse_book)
-            # yield {
-            #     "name": book.css("h3 a::text").get(),
-            #     "price": book.css(".product_price .price_color::text").get(),
-            #     "url": book.css("h3 a").attrib["href"],
-            # }
         next_page = response.css("li.next a ::attr(href)").get()
         if next_page is not None:
             if "catalogue/" in next_page:
