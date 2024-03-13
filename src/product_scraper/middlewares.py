b12b2488582ca1fe5ab60799eb1335e4ed169e30
fiery.snowflake <extinctCoder@outlook.com>
2024-03-13 18:24:59 +0000

    Mar 14, 2024, 12:24 AM

--- src/product_scraper/middlewares.py
+++ src/product_scraper/middlewares.py
@@ -119,7 +119,7 @@ class ScrapeOpsFakeUserAgentMiddleware:
         self.scrapeops_api_key = settings.get("SCRAPEOPS_API_KEY")
         self.scrapeops_endpoint = settings.get(
             "SCRAPEOPS_FAKE_USER_AGENT_ENDPOINT",
-            "http://headers.scrapeops.io/v1/user-agents?",
+            "https://headers.scrapeops.io/v1/browser-headers",
         )
         self.scrapeops_fake_user_agents_active = settings.get(
             "SCRAPEOPS_FAKE_USER_AGENT_ENABLED", False
